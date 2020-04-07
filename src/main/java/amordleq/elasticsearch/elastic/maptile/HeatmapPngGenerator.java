872529798f09da15d5e94e3349ba20bc83164431
Russell Parry <qeldroma@gmail.com>
2020-04-07 05:35:41 -0400

    add scale to heatmap

--- src/main/java/amordleq/elasticsearch/elastic/maptile/HeatmapPngGenerator.java
+++ src/main/java/amordleq/elasticsearch/elastic/maptile/HeatmapPngGenerator.java
@@ -1,6 +1,8 @@
 package amordleq.elasticsearch.elastic.maptile;
 
 import org.elasticsearch.search.aggregations.bucket.geogrid.GeoGrid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import reactor.core.Exceptions;
@@ -14,6 +16,8 @@ import java.io.IOException;
 @Profile("demo")
 @Component
 public class HeatmapPngGenerator implements PngGenerator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(HeatmapPngGenerator.class);
 
     @Override
     public byte[] generatePng(int x, int y, int z, GeoGrid geoGrid) {
@@ -29,31 +33,37 @@ public class HeatmapPngGenerator implements PngGenerator {
         g2d.setComposite(AlphaComposite.Clear);
         g2d.fillRect(0, 0, 256, 256);
 
-        g2d.setComposite(AlphaComposite.Src);
+        AlphaComposite alphaComposite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.8f);
+        g2d.setComposite(alphaComposite);
         g2d.setColor(Color.BLUE);
 
-        System.out.println("Tile bounds are:"+tileBoundingBox.getNorth()+","+tileBoundingBox.getWest()+","+tileBoundingBox.getSouth()+","+tileBoundingBox.getEast());
+        LOG.trace("Tile bounds are: {},{},{},{}", tileBoundingBox.getNorth(), tileBoundingBox.getWest(), tileBoundingBox.getSouth(), tileBoundingBox.getEast());
 
-        for(GeoGrid.Bucket bucket:geoGrid.getBuckets()){
+        for (GeoGrid.Bucket bucket : geoGrid.getBuckets()) {
             String key = bucket.getKeyAsString();
+            long docCount = bucket.getDocCount();
+            double intensityScale = docCount / (30000f / z);
+            int intensity = Math.min((int) (255 * intensityScale), 240);
+            g2d.setColor(new Color(intensity, intensity, 255));
+
             String[] zxy = key.split("/");
             int bucketZ = Integer.parseInt(zxy[0]);
             int bucketX = Integer.parseInt(zxy[1]);
             int bucketY = Integer.parseInt(zxy[2]);
 
             BoundingBox bucketBoundingBox = new BoundingBox(bucketX, bucketY, bucketZ);
-            System.out.println("Bucket bounds are:"+bucketBoundingBox.getNorth()+","+bucketBoundingBox.getWest()+","+bucketBoundingBox.getSouth()+","+bucketBoundingBox.getEast());
+            LOG.trace("Bucket bounds are:{},{},{},{}", bucketBoundingBox.getNorth(), bucketBoundingBox.getWest(), bucketBoundingBox.getSouth(), bucketBoundingBox.getEast());
             double offsetX = bucketBoundingBox.getWest() - tileBoundingBox.getWest();
             double offsetY = bucketBoundingBox.getNorth() - tileBoundingBox.getNorth();
             double width = Math.abs(bucketBoundingBox.getWest() - bucketBoundingBox.getEast());
             double height = Math.abs(bucketBoundingBox.getSouth() - bucketBoundingBox.getNorth());
-            System.out.println("Offset:"+offsetX+"/"+offsetY);
-            System.out.println("Extent:"+width+"/"+height);
+            LOG.trace("Offset:{}/{}", offsetX, offsetY);
+            LOG.trace("Extent:{}/{}", width, height);
 
-            int scaledX = Math.abs((int)(offsetX / xScale));
-            int scaledY = Math.abs((int)(offsetY / yScale));
-            int scaledWidth = Math.abs((int)(width / xScale));
-            int scaledHeight = Math.abs((int)(height / yScale));
+            int scaledX = Math.abs((int) (offsetX / xScale));
+            int scaledY = Math.abs((int) (offsetY / yScale));
+            int scaledWidth = Math.abs((int) (width / xScale));
+            int scaledHeight = Math.abs((int) (height / yScale));
 
             g2d.fillRect(scaledX, scaledY, scaledWidth, scaledHeight);
         }
